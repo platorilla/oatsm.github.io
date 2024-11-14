f75159c345b3d0aa63e7ab61d24f163ebf28d85e
Gino Latorilla <platorilla@itrsgroup.com>
2024-11-14 14:52:11 +0800

    Labelled checkbox component

---
+++ client/src/CheckBox.tsx
@@ -0,0 +1,21 @@
+interface CheckboxProps {
+    label: string;
+    checked: boolean;
+    onChange: (checked: boolean) => void;
+}
+
+const Checkbox = ({ label, checked, onChange }: CheckboxProps) => {
+    return (
+        <label className="inline-flex items-center space-x-2">
+            <input
+                type="checkbox"
+                className="form-checkbox h-5 w-5 text-blue-600"
+                checked={checked}
+                onChange={(e) => onChange(e.target.checked)}
+            />
+            <span className="text-gray-700">{label}</span>
+        </label>
+    );
+}
+
+export default Checkbox;
